d540464cdaf3b2309a6a6c25af4ba8bcbba7cff2
Alex James Guinn <alexguinn47@gmail.com>
2024-06-28 13:32:33 -0600

    starting pieceMoves Function

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,7 +53,10 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        if(board.getPiece(myPosition).getPieceType() == PieceType.BISHOP){
+
+        }
+        return null;
     }
 
     @Override

d1cb5f1d4d3081e7fd07b9cc65f823d3ca293eaf
Alex James Guinn <alexguinn47@gmail.com>
2024-06-28 14:10:18 -0600

    Created a class that BishopMove can extend to hopefully avoid duplicating code between peices

--- shared/src/main/java/chess/chessMoveCalculators/BishopMove.java
+++ shared/src/main/java/chess/chessMoveCalculators/BishopMove.java
@@ -3,7 +3,7 @@ package chess.chessMoveCalculators;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class BishopMove implements PieceMove {
+public class BishopMove extends PieceMoveCalculator implements PieceMove{
     public Collection<chess.ChessMove> currentPieceMoves(chess.ChessBoard currentBoard, chess.ChessPosition currentPosition){
 
     }

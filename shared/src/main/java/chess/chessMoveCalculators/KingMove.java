5a41b3622d625e95d6312d14e72e2c79da8ba6c1
Alex James Guinn <alexguinn47@gmail.com>
2024-07-01 20:30:13 -0600

    Added classes for the rest of the pieces move calculators

---
+++ shared/src/main/java/chess/chessMoveCalculators/KingMove.java
@@ -0,0 +1,57 @@
+package chess.chessMoveCalculators;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class KingMove extends PieceMoveCalculator implements PieceMove{
+
+    public Collection<ChessMove> currentPieceMoves(ChessBoard currentBoard, ChessPosition currentPosition){
+        HashSet<ChessMove> movesHashSet = new HashSet<>();
+        int[][] movableDirections = kingMovableDirections();
+        ChessPosition initialPosition = new ChessPosition(currentPosition.getRow(),currentPosition.getColumn());
+        ChessPosition[] chessPositions = new ChessPosition[]{initialPosition,currentPosition};
+        for (int i=0; i<4; i++){
+            while(withinBoard(chessPositions[1],movableDirections[i])){
+                movesHashSet.add(pieceMoveAdder(chessPositions, movableDirections[i], currentBoard)); //add the possible move
+                if(!currentBoard.isSpaceEmpty(chessPositions[1])){break;}
+            }
+            chessPositions[1] = chessPositions[0];
+        }
+        movesHashSet.remove(null);
+        return movesHashSet;
+    }
+
+    int[][] kingMovableDirections(){
+        int[][] directions = new int[8][2];
+        //SW
+        directions[0][0] = -1;
+        directions[0][1] = -1;
+        //SE
+        directions[1][0] = -1;
+        directions[1][1] = 1;
+        //S
+        directions[2][0] = -1;
+        directions[2][1] = 0;
+        //N
+        directions[3][0] = 1;
+        directions[3][1] = 0;
+        //NE
+        directions[4][0] = 1;
+        directions[4][1] = 1;
+        //NW
+        directions[5][0] = 1;
+        directions[5][1] = -1;
+        //E
+        directions[6][0] = 0;
+        directions[6][1] = 1;
+        //W
+        directions[7][0] = 0;
+        directions[7][1] = -1;
+        return directions;
+    }
+
+}
---
+++ shared/src/main/java/chess/chessMoveCalculators/QueenMove.java
@@ -0,0 +1,45 @@
+package chess.chessMoveCalculators;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class QueenMove extends PieceMoveCalculator implements PieceMove{
+
+    public Collection<ChessMove> currentPieceMoves(ChessBoard currentBoard, ChessPosition currentPosition){
+        HashSet<ChessMove> movesHashSet = new HashSet<>();
+        int[][] movableDirections = queenMovableDirections();
+        ChessPosition initialPosition = new ChessPosition(currentPosition.getRow(),currentPosition.getColumn());
+        ChessPosition[] chessPositions = new ChessPosition[]{initialPosition,currentPosition};
+        for (int i=0; i<4; i++){
+            while(withinBoard(chessPositions[1],movableDirections[i])){
+                movesHashSet.add(pieceMoveAdder(chessPositions, movableDirections[i], currentBoard)); //add the possible move
+                if(!currentBoard.isSpaceEmpty(chessPositions[1])){break;}
+            }
+            chessPositions[1] = chessPositions[0];
+        }
+        movesHashSet.remove(null);
+        return movesHashSet;
+    }
+
+    int[][] queenMovableDirections(){
+        int[][] directions = new int[4][2];
+        //SW
+        directions[0][0] = -1;
+        directions[0][1] = -1;
+        //SE
+        directions[1][0] = -1;
+        directions[1][1] = 1;
+        //NE
+        directions[2][0] = 1;
+        directions[2][1] = 1;
+        //NW
+        directions[3][0] = 1;
+        directions[3][1] = -1;
+        return directions;
+    }
+
+}

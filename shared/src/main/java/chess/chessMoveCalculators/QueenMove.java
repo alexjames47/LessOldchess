00b5dfca8bb92bf1b87017e7e372fde6be856cc5
Alex James Guinn <alexguinn47@gmail.com>
2024-07-01 21:00:00 -0600

    Queen now functions

--- shared/src/main/java/chess/chessMoveCalculators/QueenMove.java
+++ shared/src/main/java/chess/chessMoveCalculators/QueenMove.java
@@ -14,7 +14,7 @@ public class QueenMove extends PieceMoveCalculator implements PieceMove{
         int[][] movableDirections = queenMovableDirections();
         ChessPosition initialPosition = new ChessPosition(currentPosition.getRow(),currentPosition.getColumn());
         ChessPosition[] chessPositions = new ChessPosition[]{initialPosition,currentPosition};
-        for (int i=0; i<4; i++){
+        for (int i=0; i<8; i++){
             while(withinBoard(chessPositions[1],movableDirections[i])){
                 movesHashSet.add(pieceMoveAdder(chessPositions, movableDirections[i], currentBoard)); //add the possible move
                 if(!currentBoard.isSpaceEmpty(chessPositions[1])){break;}
@@ -26,19 +26,31 @@ public class QueenMove extends PieceMoveCalculator implements PieceMove{
     }
 
     int[][] queenMovableDirections(){
-        int[][] directions = new int[4][2];
-        //SW
-        directions[0][0] = -1;
-        directions[0][1] = -1;
-        //SE
+        int[][] directions = new int[8][2];
+        //N
+        directions[0][0] = 1;
+        directions[0][1] = 0;
+        //S
         directions[1][0] = -1;
-        directions[1][1] = 1;
-        //NE
-        directions[2][0] = 1;
+        directions[1][1] = 0;
+        //E
+        directions[2][0] = 0;
         directions[2][1] = 1;
-        //NW
-        directions[3][0] = 1;
+        //W
+        directions[3][0] = 0;
         directions[3][1] = -1;
+        //SW
+        directions[4][0] = -1;
+        directions[4][1] = -1;
+        //SE
+        directions[5][0] = -1;
+        directions[5][1] = 1;
+        //NE
+        directions[6][0] = 1;
+        directions[6][1] = 1;
+        //NW
+        directions[7][0] = 1;
+        directions[7][1] = -1;
         return directions;
     }
 

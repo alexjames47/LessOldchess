fe6bfb8900874df83aa6de883f49e6463db5cd76
Alex James Guinn <alexguinn47@gmail.com>
2024-07-26 13:31:50 -0600

    running on local host

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,24 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+            var port = 8080;
+            if (args.length >= 1) {
+                port = Integer.parseInt(args[0]);
+            }
+
+            //var service = new Service(new MySqlDataAccess());
+            var server = new Server().run(port);
+            System.out.printf("Server started on port %d%n", port);
+            return;
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
+        System.out.println("""
+                Pet Server:
+                java ServerMain <port> [<dburl> <dbuser> <dbpassword> <dbname>]
+                """);
     }
 }

569f23d80d81b23935d305fb4351d061a042fe62
Alex James Guinn <alexguinn47@gmail.com>
2024-07-29 14:28:12 -0600

    wrote functions for AuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
+import exception.ResponseException;
+import model.AuthData;
+
 public interface AuthDAO {
+    AuthData createAuth() throws ResponseException;
+    AuthData getAuth(AuthData authToken) throws ResponseException;
 }

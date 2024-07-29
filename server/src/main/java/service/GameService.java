9a7897bfd97f4efbe6683a252f4eb823917c3d8b
Alex James Guinn <alexguinn47@gmail.com>
2024-07-29 14:07:02 -0600

    added functions to the services

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.GameData;
+
 public class GameService {
+
+    public GameData createGame(GameData gameName) {
+        return null;
+    }
+
+    public void joinGame(GameData playerColorAndGameID) {
+
+    }
+
+    public GameData listGames(){
+        return null;
+    }
 }
